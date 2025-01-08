4f1edf687cd507950e3a4de7cf3b261e667811a8
Datthatreya-Bhatt <datthatreyabhatt98@gmail.com>
2025-01-08 22:36:33 +0530

    Doing Network calls and andling response the right way

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,53 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { uiAction } from "./store/ui-slice";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartVisible);
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiAction.showNotification({
+          status: "pending",
+          title: "Sending....",
+          message: "Sending cart data",
+        })
+      );
+      const response = await fetch(`http://localhost:3000/addProduct`, {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      });
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+
+      dispatch(
+        uiAction.showNotification({
+          status: "Success",
+          title: "Success....",
+          message: "Sending cart data successfully",
+        })
+      );
+
+      const responseData = await response.json();
+    };
+    sendCartData().catch((error) => {
+      dispatch(
+        uiAction.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed",
+        })
+      );
+    });
+  }, [cart, dispatch]);
+
   return (
     <Layout>
       {showCart && <Cart />}
